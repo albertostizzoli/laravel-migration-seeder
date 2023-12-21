e835bcd3c633deca53b7fdca7f1ae5641ba823a5
albertostizzoli <albertostizzoli@yahoo.it>
2023-12-21 18:01:17 +0100

    create table for trains

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,17 @@
 @section('title', 'Home')
 
 @section('content')
-<main>
-    <h1>Home page</h1>
-</main>
+    <main>
+        <h1 class="text-bg-danger text-center p-2">Stazione dei treni</h1>
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-12">
+                    <a class="btn btn-primary p-3" {{ Route::currentRouteName() == 'trains.index' ? 'active' : '' }}
+                        href="{{ route('trains.index') }}"> VAI AI TRENI</a>
+                </div>
+            </div>
+        </div>
+
+    </main>
 
 @endsection
--- resources/views/trains/index.blade.php
+++ resources/views/trains/index.blade.php
@@ -3,20 +3,39 @@
 @section('title')
 
 @section('content')
-
+    <h1 class="text-center text-bg-danger p-2">Treni</h1>
     <main>
-        <h1 class="text-center">Trains</h1>
-        <div class="row">
-            @foreach ($trains as $train)
-                <div class="col-12 col-md-3 col-lg-4">
-                    <ul>
-                        <li>Azienda:{{ $train->company }}</li>
-                        <li>Stazione di partenza:{{ $train->departure_station }}</li>
-                        <li>Stazione di arrivo:{{ $train->arrival_station }}</li>
-                        <li>Orario di partenza:{{ $train->departure }}</li>
-                        <li>Orario di arrivo:{{ $train->arrived }}</li>
-                    </ul>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <table class="table mt-4">
+                        <thead>
+                            <tr class="table-danger">
+                                <th>Azienda</th>
+                                <th>Stazione di partenza</th>
+                                <th>Stazione di arrivo</th>
+                                <th>Orario di partenza</th>
+                                <th>Orario di arrivo</th>
+                                <th>Codice Treno</th>
+                                <th>Numero Carrozze</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($trains as $train)
+                                <tr class="table-light">
+                                    <th>{{ $train->company }}</th>
+                                    <td>{{ $train->departure_station }}</td>
+                                    <td>{{ $train->arrival_station }}</td>
+                                    <td>{{ $train->departure }}</td>
+                                    <td>{{ $train->arrived }}</td>
+                                    <td>{{ $train->train_code }}</td>
+                                    <td>{{ $train->number_carriages }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
-            @endforeach
+            </div>
         </div>
     </main>
+@endsection
