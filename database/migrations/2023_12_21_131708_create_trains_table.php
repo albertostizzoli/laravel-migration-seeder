7652192306ae7c6c4c058ca2b7f2eea241d1507c
albertostizzoli <albertostizzoli@yahoo.it>
2023-12-21 14:49:51 +0100

    launched migration

--- database/migrations/2023_12_21_131708_create_trains_table.php
+++ database/migrations/2023_12_21_131708_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 100);
+            $table->string('departure_station',100);
+            $table->string('arrival_station',100);
+            $table->time('departure');
+            $table->time('arrived');
+            $table->string('train_code', 10);
+            $table->smallInteger('number_carriages')->unsigned();
+            $table->boolean('in_time')->default(1);
+            $table->boolean('deleted')->default(1);
             $table->timestamps();
         });
     }
